39db4dd67bf1e8afa5fbcc04ab898cabbc1eb3ff
jophy-mj <74651662+jophy-mj@users.noreply.github.com>
2021-02-02 22:32:44 +0530

    Update p4.py

--- c2/p4.py
+++ c2/p4.py
@@ -1,10 +1,11 @@
-word=input("Enter the sentence:")
-count=0
-l=word.split(" ")
-print(l)
-for i in l:
-    for j in l:
-        if i==j:
-            count+=1
+def word_count(str):
+    counts=dict()
+    words=str.split()
+    for w in words:
+        if w in counts:
+            counts[w]+=1
+        else:
+            counts[w]=1
+    return counts
+print(word_count('a friend in need is a friend in deed.'))
 
-            
