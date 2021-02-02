c60f2a2fa1cb58b950014f463b751e925df2b3c4
jophy-mj <74651662+jophy-mj@users.noreply.github.com>
2021-02-02 22:31:22 +0530

    Update p3b.py

--- c2/p3b.py
+++ c2/p3b.py
@@ -1,3 +1,9 @@
-n=int(input("Enter the limit:"))
-l=[i*i for i in range(1,n+1)]
-print(l)
+n=int(input("enter the limit:"))
+l=[]
+for i in range(0,n):
+  num=int(input("enter no:"))
+  l.append(num)
+print("Entered list:",l)
+l1=[i*i for i in l]
+print("List of squres:",l1) 
+
